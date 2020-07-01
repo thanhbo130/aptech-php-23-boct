81bfe9650a928b625eab37593a1a067ae0f10d6e
thanhbo13o <caothanhbo@gmail.com>
2020-07-01 05:53:33 -0700

    upadated php09

---
+++ PHP/08-20200629/project08/app/Http/Controllers/MyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function index(){
+        echo "Hello url def";
+    }
+}
